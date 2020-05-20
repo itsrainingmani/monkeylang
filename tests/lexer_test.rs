7726ffa4bbf9a6f802d3d47943eabcc2bfc71682
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-05-19 23:00:56 -0400

    Expand test input to match new keywords

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -142,6 +142,14 @@ fn test_read_expanded_source() {
             x + y;
         };
         let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
+
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
         ",
     ));
 
@@ -289,6 +297,122 @@ fn test_read_expanded_source() {
         token::Token {
             kind: TokenType::SEMICOLON,
             literal: ";".to_string(),
+        },
+        token::Token {
+            kind: TokenType::BANG,
+            literal: "!".to_string(),
+        },
+        token::Token {
+            kind: TokenType::MINUS,
+            literal: "-".to_string(),
+        },
+        token::Token {
+            kind: TokenType::SLASH,
+            literal: "/".to_string(),
+        },
+        token::Token {
+            kind: TokenType::ASTERISK,
+            literal: "*".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "5".to_string(),
+        },
+        token::Token {
+            kind: TokenType::SEMICOLON,
+            literal: ";".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "5".to_string(),
+        },
+        token::Token {
+            kind: TokenType::LT,
+            literal: "<".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "10".to_string(),
+        },
+        token::Token {
+            kind: TokenType::GT,
+            literal: ">".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "5".to_string(),
+        },
+        token::Token {
+            kind: TokenType::SEMICOLON,
+            literal: ";".to_string(),
+        },
+        token::Token {
+            kind: TokenType::IF,
+            literal: "if".to_string(),
+        },
+        token::Token {
+            kind: TokenType::LPAREN,
+            literal: "(".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "5".to_string(),
+        },
+        token::Token {
+            kind: TokenType::LT,
+            literal: "<".to_string(),
+        },
+        token::Token {
+            kind: TokenType::INT,
+            literal: "10".to_string(),
+        },
+        token::Token {
+            kind: TokenType::RPAREN,
+            literal: ")".to_string(),
+        },
+        token::Token {
+            kind: TokenType::LBRACE,
+            literal: "{".to_string(),
+        },
+        token::Token {
+            kind: TokenType::RETURN,
+            literal: "return".to_string(),
+        },
+        token::Token {
+            kind: TokenType::TRUE,
+            literal: "true".to_string(),
+        },
+        token::Token {
+            kind: TokenType::SEMICOLON,
+            literal: ";".to_string(),
+        },
+        token::Token {
+            kind: TokenType::RBRACE,
+            literal: "}".to_string(),
+        },
+        token::Token {
+            kind: TokenType::ELSE,
+            literal: "else".to_string(),
+        },
+        token::Token {
+            kind: TokenType::LBRACE,
+            literal: "{".to_string(),
+        },
+        token::Token {
+            kind: TokenType::RETURN,
+            literal: "return".to_string(),
+        },
+        token::Token {
+            kind: TokenType::FALSE,
+            literal: "false".to_string(),
+        },
+        token::Token {
+            kind: TokenType::SEMICOLON,
+            literal: ";".to_string(),
+        },
+        token::Token {
+            kind: TokenType::RBRACE,
+            literal: "}".to_string(),
         },
     ];
 
